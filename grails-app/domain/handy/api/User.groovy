b111de68a38530f4211e2a059ade3f03c848d319
Andrés Díaz <diazandres756@gmail.com>
2024-09-06 13:23:30 -0500

    Se completa la primera tarea con la creación de las entidades de User y Role, con sus respectivos atributos

--- grails-app/domain/handy/api/User.groovy
+++ grails-app/domain/handy/api/User.groovy
@@ -11,22 +11,35 @@ class User {
     String phone
     String address
     String preferences
-    String active = true
+    Boolean is_register = false
 
     static hasMany = [roles: Role]
     static belongsTo = Role
 
     static mapping = {
         roles joinTable: [name: 'role_user', key: 'user_id', column: 'role_id']
-        table 'customer'
+        table 't_user'
         id column: 'identification'
     }
 
     static constraints = {
         name blank: false
+        lastname blank: false
+        username blank: false
         email email: true, blank: false, unique: true
         phone matches: "\\d{10}", blank: false
         address nullable: true
         preferences nullable: true
+        password nullable: true, blank: false, validator: { val, obj ->
+            if (obj.is_register && (!val || !obj.validatePassword(val))) {
+                return ['invalidPassword']
+            }
+        }
+    }
+
+    def validatePassword(String password){
+        def passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/
+
+        return password.matches(passwordPattern)
     }
 }
