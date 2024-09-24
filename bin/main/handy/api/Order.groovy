bec21ccc7d6e16aec6a6d28955ceef0f99353bfe
JBMjese <6935@holbertonstudents.com>
2024-09-24 09:29:21 -0500

    Guardar cambios locales antes del pull

---
+++ bin/main/handy/api/Order.groovy
@@ -0,0 +1,30 @@
+package handy.api
+
+class Order {
+    Integer id
+    String order_description
+    Integer id_client
+    String order_status
+    BigDecimal discount
+    BigDecimal subtotal
+    BigDecimal total
+    Date create_at = new Date()
+    Date update_at  = new Date()
+    static hasMany = [productsOrder: ProductOrder] // Relación muchos a muchos a través de OrderProduct
+    static belongsTo = Product
+
+    static mapping = {
+        version false
+        table 't_order'
+        products joinTable: [name: 'product_order', key: 'order_id', column: 'product_id']
+    }
+
+    static constraints = {
+        id_client nullable: true // Permitir que sea nulo
+        order_description nullable: false, blank: false
+        order_status nullable: false, blank: false
+        discount nullable: false, min: 0.0
+        subtotal nullable: false, min: 0.0
+        total nullable: false, min: 0.0
+    }
+}
--- bin/main/handy/api/UrlMappings.groovy
+++ bin/main/handy/api/UrlMappings.groovy
@@ -10,17 +10,47 @@ class UrlMappings {
         post "/$controller(.$format)?"(action: "save")
         put "/$controller/$id(.$format)?"(action: "update")
         patch "/$controller/$id(.$format)?"(action: "patch")
-        
+
         // Rutas personalizadas para User
         "/saveUser"(controller: "User", action: "save", method: "POST")
-        "/updateUser/$id"(controller: "User", action: "update", method: "PUT")  
+        "/updateUser/$id"(controller: "User", action: "update", method: "PUT")
         "/deleteUser/$id"(controller: "User", action: "delete", method: "DELETE")
-        "/deactivateUser/$id"(controller: "User", action: "deactivate", method: "PUT")     
+        "/deactivateUser/$id"(controller: "User", action: "deactivate", method: "PUT")
         "/searchUsers"(controller: "User", action: "searchUsers", method: "POST")
-        
+
         // Página principal y manejo de errores
+        delete "/$controller/$id(.$format)?"(action:"delete")
+        get "/$controller(.$format)?"(action:"index")
+        get "/$controller/$id(.$format)?"(action:"show")
+        post "/$controller(.$format)?"(action:"save")
+        "/saveOrder"(controller: "Order", action: "save", method: "POST")
+        "/Order/$id"(controller: 'Order', action: 'show') {
+            constraints {
+                id(matches: /\d+/) // Asegura que el ID sea un número
+            }
+        }
+        "/updateOrder/$id"(controller: "Order", action: "update") {
+            constraints {
+                id(matches: /\d+/) // Asegura que el ID sea un número
+            }
+        }
+
+            "/updateStateOrder/$id"(controller: "Order", action: "updateState") {
+            constraints {
+                id(matches: /\d+/) // Asegura que el ID sea un número
+            }
+        }
+
+        put "/$controller/$id(.$format)?"(action:"update")
+        patch "/$controller/$id(.$format)?"(action:"patch")
         "/"(controller: 'application', action:'index')
         "500"(view: '/error')
         "404"(view: '/notFound')
+
+        "/auth/register"(controller: "userRegistration", action: "register", method: "POST")
+
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+
     }
 }
