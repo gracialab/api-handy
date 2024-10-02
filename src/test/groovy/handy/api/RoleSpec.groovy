f175310612d5930eb10250d20a3ca63ec61a998f
Andrés Díaz <diazandres756@gmail.com>
2024-10-02 09:47:05 -0500

    Se realizo pruebas unitarias a los servicios de roles

--- src/test/groovy/handy/api/RoleSpec.groovy
+++ src/test/groovy/handy/api/RoleSpec.groovy
@@ -6,11 +6,17 @@ import spock.lang.Specification
 class RoleSpec extends Specification implements DomainUnitTest<Role> {
 
      void "test domain constraints"() {
-        when:
+        when: "A role is valid"
         Role domain = new Role(name: "Admin", description: "admin")
-        //TODO: Set domain props here
-
-        then:
+        then: "the role is validated"
         domain.validate()
      }
+
+    void "test role name is empty"(){
+        when: "name is empty"
+        Role role = new Role(description: "admin")
+        then: "validation throws error"
+        !role.validate()
+        role.errors['name'].codes.any {it.contains('nullable')}
+    }
 }
